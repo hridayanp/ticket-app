e254b68b9c684fd15e504ef68b85505ce6b47b84
hridayanp <hridayanphukan123@gmail.com>
2023-02-04 21:16:29 +0530

    updated import statements and common module

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,9 +2,8 @@ import jwt from 'jsonwebtoken';
 import { Password } from './../services/password';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { validateRequest } from '../middlewares/validate-request';
+import { validateRequest, BadRequestError } from '@hpticketing/gittix-common';
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
