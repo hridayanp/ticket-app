19620cff17b218543b02a9a1d6f099df6c96e4af
hridayanp <hridayanphukan123@gmail.com>
2023-02-05 19:53:39 +0530

    added crud in ticket service and tests around the service

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,10 +1,9 @@
 export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
+export * from './errors/not-authorised-error';
 export * from './errors/not-found-error';
-// export * from './errors/forbidden-request-error';
 export * from './errors/request-validation-error';
-// export * from './errors/unauthorized-request-error';
 
 export * from './middlewares/validate-request';
 export * from './middlewares/error-handler';
